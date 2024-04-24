584df0f505001a5a772a588e058d62311f0e8671
Anze <anze.repse@gmail.com>
2024-04-24 16:59:06 +0200

    beta2

--- src/app/_components/Zacetek_slika.tsx
+++ src/app/_components/Zacetek_slika.tsx
@@ -14,12 +14,12 @@ export default function Component() {
                         <h1 className="text-4xl md:text-6xl font-bold tracking-tight">Zanesljivo pod vašimi nogami </h1>
                         <p className="text-lg md:text-xl">Parketarstvo Ravbar z vami že od leta 1996</p>
                         <div className="flex flex-col sm:flex-row gap-4 justify-center">
-                            <Link
+                            {/* <Link
                                 className="inline-flex items-center justify-center rounded-md bg-primary px-8 py-3 text-base font-medium text-white shadow-sm transition-colors hover:bg-primary/80 focus:outline-none focus:ring-2 focus:ring-primary focus:ring-offset-2"
                                 href="#"
                             >
                                 Shop Flooring
-                            </Link>
+                            </Link> */}
                             <Link
                                 className="inline-flex items-center justify-center rounded-md border border-white px-8 py-3 text-base font-medium text-white transition-colors hover:bg-white hover:text-primary focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2"
                                 href="#"
